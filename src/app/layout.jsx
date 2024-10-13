0e1c79aaaaa500c52304b03f73a96d621eedaf6f
Dennar David <dennardavid@gmail.com>
2024-10-13 21:01:00 +0100

    initial folder structure scaffold

---
+++ src/app/layout.jsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "MakeWeWork",
+  description:
+    "MakeWeWork is a platform that aims to bridge the gap between unexperienced techies and their first job.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
