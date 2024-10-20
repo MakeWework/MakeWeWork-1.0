216d10e4898460a64d80092cccf4cb89c9537397
Damilola Akinlolu <confinesofgrace@gmail.com>
2024-10-20 14:29:31 +0100

    Finishing the scaffolding for B section

--- src/app/Components/SectionB.jsx
+++ src/app/Components/SectionB.jsx
@@ -3,17 +3,31 @@ function SectionB() {
         <div id="body-sectionB">
 
             <div id="content-sectionB">
-                <div id="frame-sectionB">i</div>
+                <div id="frame-sectionB"></div>
                 <div id="text-sectionB">
                     <div id="title-sectionB">
                         <h3>
                             <span>MakeWeWork</span> is bridging the gap between education and career success
                         </h3>
 
+                    </div>
                         <p>
                             Our job simulations build hands-on skills for real-life jobs and a project tracking system to get techies ready for the job.
                         </p>
-                    </div>
+
+                        <ul>
+                                <li>Get registered </li>
+                                <li>Select a job sim. </li>
+                                <li>Complete a project </li>
+                                <li>Earn a badge </li>
+                                <li>Get featured and have a chance to connect with recruiters </li>
+                        </ul>
+
+                        <div id="sectionB-btns">
+                            <button id="btn1"> Sign up for free</button>
+                            <button id="btn2"> Learn more</button>
+                        </div>
+                    
                 </div>
             </div>
         </div>
