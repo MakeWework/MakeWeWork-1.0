9117570d6d60bb517df43e49b548cd47cb79a9a4
Damilola Akinlolu <confinesofgrace@gmail.com>
2024-10-20 11:22:37 +0100

    Scaffolding B Section

--- src/app/Components/SectionB.jsx
+++ src/app/Components/SectionB.jsx
@@ -1,7 +1,21 @@
 function SectionB() {
     return (
-        <div>
-            This is the Second section
+        <div id="body-sectionB">
+
+            <div id="content-sectionB">
+                <div id="frame-sectionB">i</div>
+                <div id="text-sectionB">
+                    <div id="title-sectionB">
+                        <h3>
+                            <span>MakeWeWork</span> is bridging the gap between education and career success
+                        </h3>
+
+                        <p>
+                            Our job simulations build hands-on skills for real-life jobs and a project tracking system to get techies ready for the job.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
